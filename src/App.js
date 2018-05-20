89e6e49c5a5a96fa759571dbbccb03738997366f
antonror <hitty@i.ua>
2018-05-20 20:37:08 +0300

    basic search

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import './App.css'
 import PokemonsContainer from './components/PokemonsContainer'
+import PokemonSearch from './components/PokemonSearch'
 
 class App extends Component {
     render() {
@@ -9,6 +10,7 @@ class App extends Component {
                 <div className="App-header">
                     <h1>Welcome to Pokemon Dashboard</h1>
                 </div>
+                <PokemonSearch />
                 <div className="poke-lineup">
                     <div className="component-header"><h2>Pokemon Lineup</h2></div>
                 </div>
