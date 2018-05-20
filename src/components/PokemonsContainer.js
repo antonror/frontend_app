e728366b1e7bd86783c41cf1f1bb710639b3d2d6
antonror <hitty@i.ua>
2018-05-20 14:04:46 +0300

    Main Page with Pokemons fetched from API

---
+++ src/components/PokemonsContainer.js
@@ -0,0 +1,37 @@
+import React, { Component } from 'react'
+import axios from 'axios'
+
+class PokemonsContainer extends Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            pokemons: []
+        }
+    }
+
+    componentDidMount() {
+        axios.get('http://localhost:3001/api/v1/pokemons.json')
+            .then(response => {
+                console.log(response)
+                this.setState({pokemons: response.data})
+            })
+            .catch(error => console.log(error))
+    }
+
+    render() {
+        return (
+            <div>
+                {this.state.pokemons.map((pokemon) => {
+                    return(
+                        <div className="tile" key={pokemon.id} >
+                            <h4>{pokemon.title}</h4>
+                            <p>{pokemon.body}</p>
+                        </div>
+                    )
+                })}
+            </div>
+        )
+    }
+}
+
+export default PokemonsContainer
